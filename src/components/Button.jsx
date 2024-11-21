a46f52448301161667fb58e7bec18aa54e7298ed
mangoshoieb <159851387+mangoshoieb@users.noreply.github.com>
2024-11-21 03:20:04 +0200

    update eslint

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import clsx from "clsx";
 import {Marker} from "./Marker.jsx";
 
-const Button = ({icon,href,children,containerClassname,onClick,markFill}) => {
-    const Inner=()=>(
+const Button = ({icon, href, children, containerClassname, onClick, markFill}) => {
+    const Inner = () => (
         <>
         <span className="relative flex items-center px-4 g4 rounded-2xl
             inner-before min-h-[60px] group-hover:before:opacity-100 overflow-hidden">
             <span className="absolute -left-[1px]">
                 <Marker markfill={markFill}/>
             </span>
-            {icon&&(
+            {icon && (
                 <img src={icon} alt='circle' className='z-10 size-10 object-contain mr-5'/>
             )}
             <span className="relative z-2 text-p1 base-bold uppercase font-poppins">
@@ -20,12 +19,13 @@ const Button = ({icon,href,children,containerClassname,onClick,markFill}) => {
         </span>
         </>
     )
-    return href ?(
-        <a className={clsx("relative p-0.5 g5 shadow-500 rounded-2xl group",containerClassname)} href={href}>
+    return href ? (
+        <a className={clsx("relative p-0.5 g5 shadow-500 rounded-2xl group", containerClassname)} href={href}>
             <Inner/>
         </a>
-    ):(
-        <button className={clsx("relative p-0.5 g5 shadow-500 rounded-2xl group",containerClassname)} onClick={onClick}>
+    ) : (
+        <button className={clsx("relative p-0.5 g5 shadow-500 rounded-2xl group", containerClassname)}
+                onClick={onClick}>
             <Inner/>
         </button>
     )
